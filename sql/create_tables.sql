8b559fcfc996a9e5b3f4596ba6c30edca2e448b5
Jonne Junno <jonne.junno@helsinki.fi>
2017-03-22 13:38:49 +0200

    luotiin tietokantatauluja

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,32 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Player(
+  id SERIAL PRIMARY KEY,
+  nickname varchar(50) UNIQUE NOT NULL,
+  password varchar(50) NOT NULL,
+  created timestamp DEFAULT current_timestamp,
+  admin boolean DEFAULT FALSE
+);
+
+CREATE TABLE Area(
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES Player(id),
+  name varchar(50) NOT NULL,
+  description varchar(150) NOT NULL
+);
+
+CREATE TABLE Topic(
+  id SERIAL PRIMARY KEY,
+  area_id INTEGER REFERENCES Area(id),
+  name varchar(75) NOT NULL,
+  added timestamp DEFAULT current_timestamp,
+  modified timestamp
+);
+
+CREATE TABLE Message(
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES Player(id),
+  topic_id INTEGER REFERENCES Topic(id),
+
+  added timestamp DEFAULT current_timestamp,
+  modified timestamp
+);
