e72e06e87754f8158d9eba91ddf4dc422f0e9a8f
Jonne Junno <jonne.junno@helsinki.fi>
2017-04-05 18:29:07 +0300

    käyttäjä hommat puolihoidossa

--- app/controllers/player_controller.php
+++ app/controllers/player_controller.php
@@ -7,8 +7,19 @@
     }
 
     public static function handle_login(){
-      
+      $params = $_POST;
+
+    $player = Player::authenticate($params['nickname'], $params['password']);
+
+    if(!$player){
+      View::make('/login.html', array('error' => 'Väärä käyttäjätunnus tai salasana!', 'username' => $params['username']));
+    }else{
+      $_SESSION['player'] = $player->id;
+
+      Redirect::to('/area', array('message' => 'Tervetuloa takaisin keskustelemaan' . $player->name . '!'));
     }
+  }
+
 
 
     public static function store(){
--- app/models/player.php
+++ app/models/player.php
@@ -14,7 +14,37 @@
       $row = $query->fetch();
       $this->id = $row['id'];
 
-}
+    }
 
+    public function find_by_id($id){
+      $query = DB::connection()->prepare('SELECT * FROM Player WHERE id = :id');
+      $query->execute(array('id' => $id));
+      $row = $query->fetch();
+
+      if($row){
+        $player = new Player(array(
+          'id' => $row['id'],
+          'nickname' => $row['nickname'],
+          'password' => $row['password'],
+          'created' => $row['created'],
+          'admin' => $row['admin']
+        ));
+        return $player;
+      }
+      return null;
+    }
+
+    public function authenticate(){
+
+      $query = DB::connection()->prepare('SELECT * FROM Player WHERE nickname = :nickname AND password = :password LIMIT 1');
+      $query->execute(array('nickname' => $nickname, 'password' => $password));
+      $row = $query->fetch();
+      if($row){
+        $player = $this->find_by_id($row['id']);
+        return $player;
+      }else{
+        return null;
+      }
+    }
 
   }
